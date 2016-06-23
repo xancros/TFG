173a5c54dcc62a6814a1da73cc4f55eee68e5f15
xancros <xancros69@gmail.com>
2016-06-23 14:04:00 +0200

    Mejorar OCR Seguir avanzando en el OCR y estudiar HOG.

--- OCR.py
+++ OCR.py
@@ -2,7 +2,8 @@ from os import listdir
 
 import cv2
 import numpy as np
-from sklearn.lda import LDA
+# from sklearn.lda import LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 pathTrain = "./Training_Images/OCRImages/Train/training_ocr/"
 caracterMatrix = []
@@ -16,7 +17,8 @@ diccionario = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
                'S': 28, 'T': 29, 'U': 30, 'V': 31, 'W': 32, 'X': 33, 'Y': 34,
                'Z': 35}
 
-
+trainShape = []
+pruebaC = None
 # '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','ESP'
 
 
@@ -40,11 +42,29 @@ def entrenarOCR():
                 umbralcut = umbral[y:y + h, x:x + w]
                 caracter = cv2.resize(umbralcut, (10, 10), None, 0, 0, cv2.INTER_NEAREST)
                 caracterLine = np.asarray(caracter).ravel()
+                # letralinea.reshape(-1,1)
+                #caracterLine = caracterLine.reshape(-1,1)
                 caracterMatrix.append(caracterLine)
+                # cv2.imshow("caracter",img)
+                # cv2.imshow("umbral",umbral)
+                # cv2.imshow("umbralCut",umbralcut)
+                # cv2.waitKey()
+                # cv2.destroyAllWindows()
+    pruebaC = caracterLine
+    carShape = caracter.shape
+    trainShape.append([carShape[0], carShape[1]])
     print("hemos acabado el entrenamiento")
 
 
 def analizarImagen(imagen):
+    a, b, ch = imagen.shape
+    mid = [int(a / 2), int(b / 2)]
+    print(a, b)
+    shp = [a, b]
+    minimum = int((np.amin(shp)) / 10)
+    print(minimum)
+    maximum = int((np.amin(shp)) / 2)
+    print(maximum)
     print("busca texto STOP en la imagen")
     carMatrix = np.vstack(caracterMatrix)
     matr = []
@@ -53,11 +73,14 @@ def analizarImagen(imagen):
     y3 = 0
     y2 = 0
     E = np.array(carIndex)
-    clf.fit.transform(carMatrix, carIndex)
+    clf.fit_transform(carMatrix, carIndex)
     CR = clf.transform(carMatrix)
     CR = CR.astype(np.float32, copy=True)
+    E = E.reshape((E.size, 1))
     # Bayes = cv2.NormalBayesClassifier(CR,responses=E)
-    Knear = cv2.KNearest(CR, E, max_k=50)
+    # Knear = cv2.KNearest(CR, E, max_k=50)
+    Knear = cv2.ml.KNearest_create()
+    Knear.train(CR, cv2.ml.ROW_SAMPLE,E)
     # em = cv2.EM()
     # em.train(CR,labels=E)
     img = imagen.copy()
@@ -66,85 +89,104 @@ def analizarImagen(imagen):
     im2, contours, hierarchy = cv2.findContours(imgTH.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
     cntOrd = []
     parents = []
+    sections = []
+    subContours = []
+    subHierarchy = []
+    rectangulos = []
+    print(len(hierarchy[0]))
     for i in range(0, contours.__len__()):
-        # x,y,w,h=cv2.boundingRect(cnt)
-        # if(y>=y2 and y<y3):
-        #     if(x>=x2 and x<x3):
-        if (hierarchy[0][i][2] != -1):
-            x, y, w, h = cv2.boundingRect(contours[i])
-            if (h > (y3 - y2) / 2 and w > (x3 - x2) / 2):
-                parents.append(i)
-    for padre in parents:
-        son = hierarchy[0][padre][2]
-        indexes = [son]
-        while (hierarchy[0][son][0] != -1):
-            son = hierarchy[0][son][0]
-            indexes.append(son)
-        for i in indexes:
-            # if (contours[i].size>50):
-            x, y, w, h = cv2.boundingRect(contours[i])
-            if (contours[i].size < imgTH.size / 20):
-                # print contours[i].size
-                x, y, w, h = cv2.boundingRect(contours[i])
-                if (h > (y3 - y2) / 2.5 and w > (x3 - x2) / 26):
-                    if (w < (x3 - x2) / 7):
-                        cv2.rectangle(imagen, (x + x2, y + y2), (x + w + x2, y + h + y2), (255, 0, 255))
-                        cntOrd.append((x, y, w, h))
+        x, y, w, h = cv2.boundingRect(contours[i])
 
-    dtype = [('x', np.int32), ('y', np.int32), ('width', np.int32), ('height', np.int32)]
-    rectangulos = np.array(cntOrd, dtype=dtype)
-    rectangulos = np.sort(rectangulos, order='x')
+        distX = np.abs(w - x)
+        distY = np.abs(h - y)
+        area = distX * distY
 
-    for rect in rectangulos:
-        letra = imgTH[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
-        caracter = cv2.resize(letra, (10, 10), None, 0, 0, cv2.INTER_NEAREST)
-        letralinea = np.asarray(caracter).ravel()
-        letralinea = clf.transform(letralinea)
-        letralinea = letralinea.astype(np.float32)
-        # retval, results = Bayes.predict(np.float32(letralinea))
-        retval, results, neighbors, dists = Knear.find_nearest(np.float32(letralinea), 10)
-        # retval,probs = em.predict(np.float32(letralinea))
-        # print retval
-        for key, value in diccionario.iteritems():
-            if value == int(retval):
-                # print key
-                if value != 36:
-                    matr.append(key)
-                    cv2.putText(imagen, key, (rect[0] + x2, rect[1] - 20 + y2), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                                (0, 0, 255), lineType=cv2.CV_AA)
-                break
-                # print retval
+        if (w > minimum and h > minimum):
+            if (w < maximum and h < maximum):
+                ms = mid[1] + maximum, mid[1] - maximum
+                if (h <= ms[0] and h >= ms[1]):
+                    # cv2.rectangle(img,(x,y),(x+w,y+h),(50,255,50),1)
+                    sections.append([x, y, w, h])
+                    subContours.append(contours[i])
+                    subHierarchy.append(hierarchy[0][i])
+                    print("valor de w y h: ", (w,h))
 
-    # print results
+                    hi = hierarchy[0][i][2]
+                    if (hi != -1):
+                        x, y, w, h = cv2.boundingRect(contours[i])
+                        cv2.rectangle(imagen, (x, y), (x + w, y + h), (50, 255, 50), 1)
 
-    indice = 0
-    matrString = []
-    if (matr.__len__() != 0):
-        # print matr
-        if (not matr[0].isdigit()):
-            for indice in range(0, matr.__len__()):
-                if (indice == 8):
-                    # print matrString
-                    matrString.append(" ")
-                    # print fileName
-
-                matrString.append(matr[indice])
-        else:
-            for indice in range(0, matr.__len__()):
-
-                if (indice == 7):
-                    # print matrString
-                    matrString.append(" ")
-                    # print fileName
-
-                matrString.append(matr[indice])
-
-        print(matrString)
-        # self.listaMatriculas.append(matrString)
-    #
+                        rectangulos.append([x, y, w, h])
+                        # if (h > (y3 - y2) / 2 and w > (x3 - x2) / 2):
+                        #     parents.append(i)
+    # sorted(student_tuples, key=lambda student: student[2])
 
 
 
+
+
+    if (True):
+        ind = 0
+        for rect in rectangulos:
+            # letra = imgTH[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
+            letra = imgTH[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
+            cv2.imshow("letra", letra)
+            cv2.waitKey(200)
+            cv2.destroyAllWindows()
+            caracter = cv2.resize(letra, (10, 10), None, 0, 0, cv2.INTER_NEAREST)
+            print(caracter.shape)
+            letralinea = np.asarray(caracter).ravel()
+            # letralinea=letralinea.reshape(-1,1)
+            letralinea = clf.transform(letralinea)
+            letralinea = letralinea.astype(np.float32)
+            # retval, results = Bayes.predict(np.float32(letralinea))
+            retval, results, neighbors, dists = Knear.findNearest(letralinea, 10)
+            # retval,probs = em.predict(np.float32(letralinea))
+            print(retval)
+            for key, value in diccionario.items():
+                if value == int(retval):
+                    # print key
+                    if value != 36:
+                        matr.append(key)
+                        print(key)
+                        cv2.putText(imagen, key, (rect[0] + x2, rect[1] - 20 + y2), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                    (0, 0, 255))
+                    break
+                    # print retval
+
+        print(results)
+        if (matr.__contains__("STP")):
+            print(True)
+        indice = 0
+        matrString = []
+        if (matr.__len__() != 0):
+            # print matr
+            if (not matr[0].isdigit()):
+                for indice in range(0, matr.__len__()):
+                    if (indice == 8):
+                        # print matrString
+                        matrString.append(" ")
+                        # print fileName
+
+                    matrString.append(matr[indice])
+            else:
+                for indice in range(0, matr.__len__()):
+
+                    if (indice == 7):
+                        # print matrString
+                        matrString.append(" ")
+                        # print fileName
+
+                    matrString.append(matr[indice])
+
+            print(matrString)
+            # self.listaMatriculas.append(matrString)
+            #
+
+    cv2.circle(img, (mid[0], mid[1]), 3, (0, 0, 0), -1)
+    cv2.imshow("mmm", img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
     cv2.imshow("imagen", imagen)
     cv2.waitKey()
     cv2.destroyAllWindows()
