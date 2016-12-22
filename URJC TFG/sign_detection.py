1379ba15db146a22649aabd1241c10ad7e7372ed
xancros <xancros69@gmail.com>
2016-12-22 16:06:45 +0100

    NormalBayesClassifier entrenado basico calcula cosas para probar, Probado con imagenes de training por si clasifica o no -Creo lista con casos de prueba para validación cruzada 20% casos train

--- URJC TFG/sign_detection.py
+++ URJC TFG/sign_detection.py
@@ -23,6 +23,55 @@ res = []
 CLL = None
 CLU = None
 
+def printAreaTest():
+    area2 = []
+    for filename in os.listdir(test_dir):
+        if os.path.splitext(filename)[1].lower() in test_ext:
+            full_path = os.path.join(test_dir, filename)
+            I = cv2.imread(full_path)
+            Icopy = I.copy()
+            Igray = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+            shape = Icopy.shape
+            regions = mser.detectRegions(Igray, None)
+            rects = [cv2.boundingRect(p.reshape(-1, 1, 2)) for p in regions]
+            for r in rects:
+                x, y, w, h = r
+                # Simple aspect ratio filtering
+                aratio = float(w) / float(h)
+                if (aratio > 1.2) or (aratio < 0.8):
+                    continue
+                area = I[y:y+h,x:x+w]
+                x10,y10,w10,h10 = int(x*0.1),int(y*0.1),int(w*0.1),int(h*0.1)
+                x2,y2,w2,h2 = x-x10,y-y10,w+w10,h+h10
+                w2w=int(w2-w)
+
+                pX1,pY1,pX2,pY2 = x2,y2,x+x10+w2,y+y10+h2
+                # if(pX1<0):pX1=0
+                # elif(pX1>shape[0]):pX1=shape[0]
+                # if(pX2>shape[0]):pX2=shape[0]
+                # elif(pX2<0):pX2=0
+                # if(pY1<0):pY1=0
+                # elif(pY1>shape[1]):pY1=shape[1]
+                # if(pY2>shape[1]):pY2=shape[1]
+                # elif(pY2<0):pY2=0
+                area2 = I[pY1:pY2,pX1:pX2]
+                lenArea = np.array(area2).size
+                if(lenArea>0):
+                    cv2.rectangle(Icopy, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    cv2.rectangle(Icopy,(pX1,pY1),(pX2,pY2),(255,0,0),2)
+                    cv2.circle(Icopy,(pX1,pY1),4,(0,0,255),-1)
+                    cv2.circle(Icopy, (pX2, pY1), 4, (0, 255, 0), -1)
+                    cv2.circle(Icopy, (pX1, pY2), 4, (255, 0, 0), -1)
+                    cv2.circle(Icopy, (pX2, pY2), 4, (0, 255, 255), -1)
+                    cv2.imshow("area",area)
+                    cv2.imshow("area2",area2)
+                    cv2.imshow("imagen",Icopy)
+                    cv2.waitKey(500)
+                    cv2.destroyAllWindows()
+                    Icopy=I.copy()
+            cv2.imshow('img', Icopy)
+            cv2.waitKey(0)
+
 def testTrain():
         global windowArray
         global windowArray10
@@ -377,9 +426,11 @@ def train():
             if not res.__contains__(imagenRandom):
                 res.append(imagenRandom)
     print()
-train()
-print("----------------------")
-LDA()
-print("----------------------")
-testTrain()
-validacionCruzada()
+printAreaTest()
+# print()
+# train()
+# print("----------------------")
+# LDA()
+# print("----------------------")
+# testTrain()
+# validacionCruzada()
