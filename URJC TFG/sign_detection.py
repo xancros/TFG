09f1fd030bf2e78a2308e7cc9a9d910bd98a6107
xancros <xancros69@gmail.com>
2017-03-05 22:50:08 +0100

    NormalBayesClassifier entrenado basico calcula cosas para probar, Probado con imagenes de training por si clasifica o no -Creo lista con casos de prueba para validación cruzada 20% casos train
    --Funcional con opencv 3.2 (ahora parece que da valores?) pero está con los a prioris sin definir es decir, que hará 1/4 clases

--- URJC TFG/sign_detection.py
+++ URJC TFG/sign_detection.py
@@ -12,7 +12,7 @@ c0=0.99
 PRIORS = np.array([((1-c0)/3), ((1-c0)/3), ((1-c0)/3), c0])
 PRIORS /= PRIORS.sum()
 scala_shape=(25,25)
-scala_shape=(50,50)
+#scala_shape=(50,50)
 script_directory = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 test_dir = script_directory+"/Imagenes Deteccion/test/"
 TRAIN_DIR = script_directory+"/Imagenes Deteccion/train/"
@@ -22,8 +22,9 @@ diccionario = {'Peligro':0,'Prohibicion':1,'Stop':2,'Otros':3}
 windowArray = []
 windowArray10 = []
 labels = []
-
-clasificadorLower = lda(n_components=3,priors=PRIORS,shrinkage=None,solver="svd")
+labelsLow = []
+labelsHigh = []
+clasificadorLower = lda()
 # clasificadorLower = lda()
 clasificadorHigher = lda()
 res = []
@@ -182,6 +183,8 @@ def LDA():
     global windowArray
     global windowArray10
     global labels
+    global labelsLow
+    global labelsHigh
     global clasificadorLower
     global clasificadorHigher
     global CLL
@@ -191,19 +194,68 @@ def LDA():
     lowerWindow = np.vstack(windowArray)
     upperWindow = np.vstack(windowArray10)
     E = np.array(labels)
+    ELow = np.array(labelsLow)
+    EHigh = np.array(labelsHigh)
     # clasificadorLower.fit(lowerWindow,E)
-    vectorCLower=clasificadorLower.fit_transform(lowerWindow,E)
-    vectorCUpper=clasificadorHigher.fit_transform(upperWindow,E)
+    vectorCLower=clasificadorLower.fit_transform(lowerWindow,ELow)
+    vectorCUpper=clasificadorHigher.fit_transform(upperWindow,EHigh)
     CRU = vectorCUpper.astype(np.float32,copy=True)
     CLU = cv2.ml.NormalBayesClassifier_create()
-    CLU.train(CRU,cv2.ml.ROW_SAMPLE,E)
+    CLU.train(CRU,cv2.ml.ROW_SAMPLE,EHigh)
     CRL = vectorCLower.astype(np.float32, copy=True)
     CLL = cv2.ml.NormalBayesClassifier_create()
-    CLL.train(CRL, cv2.ml.ROW_SAMPLE, E)
+    CLL.train(CRL, cv2.ml.ROW_SAMPLE, ELow)
     print()
 
     print()
 
+
+def evaluarImagen(rutaImagen):
+    global windowArray
+    global windowArray10
+    global labels
+    global clasificadorLower
+    global clasificadorHigher
+    global CLL
+    global CLU
+    ###########################
+    probabilidades = []
+    path = (script_directory + "/salidaLower.txt")
+    file = open(path, 'w')
+    path2 = (script_directory + "/salidaHigher.txt")
+    pathPrueba = (script_directory + "/misalida.txt")
+    fileH = open(path2, 'w')
+    fileP = open(pathPrueba, "w")
+    contadorU, contadorL = 0, 0
+    imagen = cv2.imread(rutaImagen)
+    Icopy = imagen.copy()
+    Icopy2 = Icopy.copy()
+    Igray = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    shape = Icopy.shape
+    # version opencv 3.1
+    # regions = mser.detectRegions(Igray, None)
+    regions, _ = mser.detectRegions(Igray)
+    rects = [cv2.boundingRect(p.reshape(-1, 1, 2)) for p in regions]
+    for r in rects:
+        x, y, w, h = r
+        areaXYW = w * h
+        # Simple aspect ratio filtering
+        aratio = float(w) / float(h)
+        if (aratio > 1.2) or (aratio < 0.8):
+            continue
+        areaTest = Icopy[y:y + h, x:x + w]
+        senal = cv2.resize(areaTest, scala_shape, None, 0, 0, cv2.INTER_NEAREST)
+        signal = senal.ravel()
+        signalT = clasificadorLower.transform(signal)
+        vectorSignalT = signalT.astype(np.float32)
+        _, Yhat1, prob1 = CLU.predictProb(vectorSignalT)
+        indice = Yhat1[0][0]
+        if indice != 3:
+            print(indice)
+            cv2.rectangle(Icopy2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow("imagen", Icopy2)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 def validacionCruzada():
     global windowArray
@@ -218,14 +270,16 @@ def validacionCruzada():
     path = (script_directory + "/salidaLower.txt")
     file = open(path, 'w')
     path2 = (script_directory + "/salidaHigher.txt")
+    pathPrueba = (script_directory+"/misalida.txt")
     fileH = open(path2, 'w')
+    fileP=open(pathPrueba,"w")
     contadorU, contadorL = 0, 0
     for folder in os.listdir(VALIDACION_DIR):
         subF = os.path.join(VALIDACION_DIR, folder)
         for filename in os.listdir(subF):
             if os.path.splitext(filename)[1].lower() in test_ext:
                 imagen = os.path.join(subF, filename)
-                imagen = "Z:\TFG\\URJC TFG/Validacion Cruzada/00000.ppm"
+                #imagen = "Z:\TFG\\URJC TFG/Validacion Cruzada/00000.ppm"
                 test = cv2.imread(imagen)
                 testRGB = cv2.cvtColor(test.copy(), cv2.COLOR_BGR2RGB)
                 fileNameArr = imagen.split("/")
@@ -240,6 +294,7 @@ def validacionCruzada():
                 regions,_ = mser.detectRegions(Igray)
                 rects = [cv2.boundingRect(p.reshape(-1, 1, 2)) for p in regions]
                 print("Usando imagenes de train para test, processing " + imagen + "\n")
+                fileP.write("Usando imagenes de train para test, processing " + imagen + "\n")
                 file.write("Test, processing " + imagen + "\n")
                 fileH.write("Test, processing " + imagen + "\n")
                 areaFinal = [-1,-1,-1,-1]
@@ -268,12 +323,17 @@ def validacionCruzada():
                     else:
                         area2 = Icopy[y1:y1 + y2, x1:x1 + x2]
                     senal = cv2.resize(areaTest, scala_shape, None, 0, 0, cv2.INTER_NEAREST)
-                    signal = (np.asarray(senal)).reshape(1, -1)
+                    signal = senal.ravel()
                     signalT = clasificadorLower.transform(signal)
                     vectorSignalT = signalT.astype(np.float32)
                     _, Yhat1, prob1 = CLL.predictProb(vectorSignalT)
+                    senal2 = cv2.resize(areaTest,(80,80),None,0,0,cv2.INTER_NEAREST)
+                    signal2 = senal2.ravel()
+                    signalT2 = clasificadorHigher.transform(signal2)
+                    vectorSignalT2 = signalT2.astype(np.float32)
+                    _,Yhat2,prob2= CLU.predictProb(vectorSignalT2)
                     probabilidades.append(prob1)
-                    indice = Yhat1[0][0]
+                    indice = Yhat2[0][0]
                     # cv2.rectangle(Icopy2, (x, y), (x + w, y + h), (0, 255, 0), 2)
                     ##tengo que cambiar esto para que no salgan tantas lineas, sino que cuente cuantas de clase 0,1,2,3 y 4 lineas nada mas
                     if indice == 3:
@@ -296,24 +356,33 @@ def validacionCruzada():
                         # Icopy2=test.copy()
                 supuestaClase = "La imagen <<" + fileName + ">>tendria que ser categorizada como "
                 print("La imagen <<" + imagen + ">>tendría que ser categorizada como ")
-
+                fileP.write("La imagen <<" + imagen + ">>tendría que ser categorizada como ")
                 if imagen.__contains__("3-Otros"):
                     print("FONDO \n")
+                    fileP.write("FONDO \n")
                     supuestaClase+="FONDO \n"
                 elif imagen.__contains__("2-Stop"):
                     print("STOP \n")
+                    fileP.write("STOP \n")
                     supuestaClase += "STOP \n"
                 elif imagen.__contains__("1-Prohibicion"):
                     print("PROHIBICION \n")
+                    fileP.write("PROHIBICION \n")
                     supuestaClase += "PROHIBICION \n"
                 elif imagen.__contains__("0-Peligro"):
                     print("PELIGRO \n")
+                    fileP.write("PELIGRO \n")
                     supuestaClase += "PELIGRO \n"
                 print("Los resultados del clasificador son: \n")
+                fileP.write("Los resultados del clasificador son: \n")
                 print("FONDO -> " + str(otros) + "\n")
+                fileP.write("FONDO -> " + str(otros) + "\n")
                 print("STOP -> " + str(stop) + "\n")
+                fileP.write("STOP -> " + str(stop) + "\n")
                 print("PROHIBICION -> " + str(prohibicion) + "\n")
+                fileP.write("PROHIBICION -> " + str(prohibicion) + "\n")
                 print("PELIGRO -> " + str(peligro) + "\n")
+                fileP.write("PELIGRO -> " + str(peligro) + "\n")
                 ###########################
                 clases = [peligro,prohibicion, stop, otros]
                 maximo = np.amax(clases)
@@ -328,9 +397,9 @@ def validacionCruzada():
                 elif clase == 3:
                     labClase = "Otros"
 
-                cv2.imshow("imagen", Icopy2)
-                cv2.waitKey()
-                cv2.destroyAllWindows()
+                # cv2.imshow("imagen", Icopy2)
+                # cv2.waitKey()
+                # cv2.destroyAllWindows()
                 coord = [x, y, (x + w), (y + h)]
                 coordenadas = (
                 "X:" + str(areaFinal[0]) + " Y:" + str(areaFinal[1]) + " X2:" + str(areaFinal[2]) + " Y2:" + str(areaFinal[3]))
@@ -346,7 +415,8 @@ def validacionCruzada():
                 file.write("--------------------------------------" + "\n")
                 otros, peligro, stop, prohibicion = 0, 0, 0, 0
                 senal = cv2.resize(area2, scala_shape, None, 0, 0, cv2.INTER_NEAREST)
-                signal = (np.asarray(senal)).reshape(1, -1)
+                # signal = (np.asarray(senal)).reshape(1, -1)
+                signal = senal.ravel()
                 signalT = clasificadorLower.transform(signal)
                 vectorSignalT = signalT.astype(np.float32)
                 _, Yhat1, prob1 = CLL.predictProb(vectorSignalT)
@@ -395,9 +465,15 @@ def train():
     global windowArray10
     global labels
     global res
+    global labelsLow
+    global labelsHigh
     count = 0
     count2 = 0
     listaImagenes = []
+    mas50 = 0
+    menos50 = 0
+    # path = (script_directory + "/shapes.txt")
+    # file = open(path, 'w')
     numOtros, numStop,numPeligro,numPro = 0,0,0,0
     for folder in os.listdir(TRAIN_DIR):
         parcial_path = os.path.join(TRAIN_DIR, folder)
@@ -439,7 +515,7 @@ def train():
                         #### LLAMADA A PROCESO LDA
                         window = Icopy[max[1]:max[1] + max[3], max[0]:max[0] + max[2]]
                         # windowArray.append(window)
-                        window = cv2.resize(window, scala_shape, None, 0, 0, cv2.INTER_NEAREST)
+
                         lab = None
                         if subF.__contains__("Peligro"):
                             lab = "Peligro"
@@ -454,19 +530,40 @@ def train():
                             lab = "Otros"
                             numOtros += 1
                         labels.append(diccionario.get(lab))
-                        windowArray.append(window.ravel())
+                        if I.shape[0] > 50 and I.shape[1] > 50:
+                            window = cv2.resize(window, scala_shape, None, 0, 0, cv2.INTER_NEAREST)
+                            windowArray.append(window.ravel())
+                            labelsLow.append(diccionario.get(lab))
+                        else:
+                            window = cv2.resize(window, (80,80), None, 0, 0, cv2.INTER_NEAREST)
+                            if area2[0]<0 or area2[1]<0 or area2[2]>shape[0] or area2[3]>shape[1]:
+                                windowArray10.append(window.ravel())
+                            else:
+                                windowEXT = Icopy[area2[1]:area2[1] + area2[3], area2[0]:area2[0] + area2[2]]
+                                windowEXT = cv2.resize(windowEXT, (80,80), None, 0, 0, cv2.INTER_NEAREST)
+                                windowArray10.append(windowEXT.ravel())
+                            labelsHigh.append(diccionario.get(lab))
                         # cv2.imshow('img', window)
                         #
                         # cv2.waitKey(800)
                         # cv2.destroyAllWindows()
-                        if area2[0]<0 or area2[1]<0 or area2[2]>shape[0] or area2[3]>shape[1]:
-                            windowArray10.append(window.ravel())
-                        else:
-                            windowEXT = Icopy[area2[1]:area2[1] + area2[3], area2[0]:area2[0] + area2[2]]
-                            windowEXT = cv2.resize(windowEXT, scala_shape, None, 0, 0, cv2.INTER_NEAREST)
-                            windowArray10.append(windowEXT.ravel())
+                        # if area2[0]<0 or area2[1]<0 or area2[2]>shape[0] or area2[3]>shape[1]:
+                        #     windowArray10.append(window.ravel())
+                        # else:
+                        #     windowEXT = Icopy[area2[1]:area2[1] + area2[3], area2[0]:area2[0] + area2[2]]
+                        #     windowEXT = cv2.resize(windowEXT, scala_shape, None, 0, 0, cv2.INTER_NEAREST)
+                        #     windowArray10.append(windowEXT.ravel())
+                        if(I.shape[0]>50 and I.shape[1]>50):
+                            mas50+=1
+                        elif (I.shape[0]<=50 and I.shape[1]<=50):
+                            menos50+=1
+                        # file.write(full_path+"\n"+str(I.shape)+"\n")
+
                         count+=1
                     listaImagenes.append(subF+"\\"+filename)
+    # file.write("imagenes con shape > 50 = "+str(mas50))
+    # file.write(" imagenes con shape <= 50 = " + str(menos50))
+    # file.close()
                         # if count == 1:
                         #     return
                         # count+=1
